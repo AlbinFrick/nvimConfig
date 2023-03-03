0f6c68ad27483ce08e11dceb8c53a1f32a43dd72
AlbinFrick <abbzez@gmail.com>
2023-03-03 08:53:33 +0100

    remove unused highlights

--- lua/albinfrick/highlights.lua
+++ lua/albinfrick/highlights.lua
@@ -3,6 +3,3 @@ vim.opt.winblend = 0
 vim.opt.wildoptions = 'pum'
 vim.opt.pumblend = 5
 vim.opt.background = 'dark'
-
-hi! TermCursor guifg=NONE guibg=#ebdbb2 gui=NONE cterm=NONE
-hi! TermCursorNC guifg=#ebdbb2 guibg=#3c3836 gui=NONE cterm=NONE
